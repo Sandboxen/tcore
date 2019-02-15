f104b6c7262ad421a6a35e77f752f447da426e2c
Tomekb530 <tomekb530@gmail.com>
2019-02-15 13:10:40 +0100

    wrong statement

--- lua/tcore/ulx/server/log.lua
+++ lua/tcore/ulx/server/log.lua
@@ -1,5 +1,5 @@
 function logtool(ply, tr, tool)
-	if tool != "inflator" or tool != "paint" then
+	if tool != "inflator" and tool != "paint" then
 		ulx.logSpawn( ply:Nick() .. "<" .. ply:SteamID() .. "> used the tool " .. tool .. " on " .. tr.Entity:GetModel() )
 	end
 end
